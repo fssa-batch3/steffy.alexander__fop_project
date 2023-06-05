e19f06d142c176fa7537eff4763758afa7b57f25
steffydominic <116252088+steffydominic@users.noreply.github.com>
2023-06-05 12:06:38 +0530

    Add files via upload

---
+++ src/day03/practice/practice01.java
@@ -0,0 +1,32 @@
+package day03.practice;
+
+public class practice01 {
+public static void main(String[] args) {
+	 int a =2;
+     System.out.println("a=" +a);
+     
+     float b =934.36f;
+     System.out.println("b=" +b);
+     
+     double c =0.000000001234;
+     System.out.println("c=" +c);
+     
+     byte d = 127;
+     System.out.println("d" +d);
+     
+     short e =30000;
+     System.out.println("e=" +e);
+     
+     long num =345376533234567876l;
+     System.out.println("num=" +num);
+     
+     
+     boolean g= true;
+     System.out.println("g=" +g);
+     
+     char mychar= 's';
+     System.out.println("mychar=" +mychar);
+     
+
+}
+}
