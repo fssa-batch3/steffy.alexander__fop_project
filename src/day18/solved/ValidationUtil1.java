e3603afe4553186a6e49310882b2650d277ec877
steffydominic <116252088+steffydominic@users.noreply.github.com>
2023-06-26 19:52:10 +0530

    Add files via upload

---
+++ src/day18/solved/ValidationUtil1.java
@@ -0,0 +1,12 @@
+package day18.solved;
+
+public class ValidationUtil1 {
+	public static boolean validateAge(int age) throws IllegalArgumentException {
+		if (age <= 0) {// Check if the age is a positive value
+			throw new IllegalArgumentException("Invalid age");
+
+		} else {
+			return true;
+		}
+	}
+}
